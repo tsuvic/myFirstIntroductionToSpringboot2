78e73043cb00cc4272e585f199d92e24dfe92f1e
Oyama <Oyama@10.27.7.17>
2021-03-26 14:53:56 +0900

    Error fixed of InquiryController

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -14,71 +14,36 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/inquiry")
 public class InquiryController {
 
- 	private final InquiryServiceImpl inquiryService;
-	
-<<<<<<< Updated upstream
-	//Add an annotation here 
-// 	public InquiryController(InquiryServiceImpl inquiryService){
-// 		this.inquiryService = inquiryService;a
-// 	}
-=======
- 	@Autowired
- 	public InquiryController(InquiryServiceImpl inquiryService){
- 		this.inquiryService = inquiryService;
- 	}
->>>>>>> Stashed changes
-	
 	@GetMapping
 	public String index(Model model) {
-		
 		//hands-on
 		return "inquiry/index";
 	}
 	
 	@GetMapping("/form")
-<<<<<<< Updated upstream
 	public String form(InquiryForm inquiryForm,
 			Model model,
 			@ModelAttribute("compelete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
-=======
-	public String form(InquiryForm inquiryForm, Model model) {
-		model.addAttribute("title", "問い合わせフォーム");
->>>>>>> Stashed changes
-		return "inquiry/form";
-	}
+		return "inquriy/form";
+	}	
 	
 	@PostMapping("/form")
 	public String formGoBack(InquiryForm inquiryForm, Model model) {
-<<<<<<< Updated upstream
-		model.addAttribute("title", "Inquiry Form");
-=======
 		model.addAttribute("title", "問い合わせフォーム");
->>>>>>> Stashed changes
 		return "inquiry/form";
 	}
 	
 	@PostMapping("/confirm")
 	public String confirm(@Validated InquiryForm inquiryForm,
-<<<<<<< Updated upstream
 			BindingResult result, 
 			Model model){
-		if(result.hasErrors()) {
-			model.addAttribute("title", "Inquiry Form");
-			return "inquiry/form";
-		}
-		model.addAttribute("title", "Confirm Page");
-=======
-			BindingResult result,
-			Model model) {
-			
 		if(result.hasErrors()) {
 		model.addAttribute("title", "問い合わせフォーム");
 		return "inquiry/form";
 		}
 		
 		model.addAttribute("title", "確認ページ");
->>>>>>> Stashed changes
 		return "inquiry/confirm";
 	}
 	
